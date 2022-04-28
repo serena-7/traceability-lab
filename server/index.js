d71208514f0025a185628beb53116a0a0192bed0
Serena Sorensen <serena.sorensen7@gmail.com>
2022-04-28 15:41:54 -0600

    troubleshooting

--- server/index.js
+++ server/index.js
@@ -16,12 +16,59 @@ var rollbar = new Rollbar({
   captureUnhandledRejections: true,
 });
 
-const {
-  getHouses,
-  deleteHouse,
-  createHouse,
-  updateHouse,
-} = require("./controller.js");
+// const {
+//   getHouses,
+//   deleteHouse,
+//   createHouse,
+//   updateHouse,
+// } = require("./controller.js");
+
+//------------
+const houses = require("./db.json");
+let houseID = 4;
+
+module.exports = {
+  getHouses: (req, res) => {
+    res.status(200).send(houses);
+  },
+  deleteHouse: (req, res) => {
+    const index = houses.findIndex((e) => e.id === +req.params.id);
+    houses.splice(index, 1);
+    res.status(200).send(houses);
+  },
+  createHouse: (req, res) => {
+    const { address, price, imageURL } = req.body;
+    let newHouse = {
+      id: houseID,
+      address,
+      price,
+      imageURL,
+    };
+    if (address === "") {
+      rollbar.error("no address added");
+    } else {
+      rollbar.log("new house added successfully");
+      houses.push(newHouse);
+      houseID++;
+      res.status(200).send(houses);
+    }
+  },
+  updateHouse: (req, res) => {
+    const id = req.params.id;
+    const type = req.body.type;
+    const index = houses.findIndex((e) => e.id === +id);
+    if (type === "minus" && houses[index].price >= 10000) {
+      houses[index].price -= 10000;
+      res.status(200).send(houses);
+    } else if (type === "plus") {
+      houses[index].price += 10000;
+      res.status(200).send(houses);
+    } else {
+      res.status(400).send("request failed");
+    }
+  },
+};
+//------------------
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));

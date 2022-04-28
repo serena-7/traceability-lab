592e401e42715aa16a0f0d7d01b49bea697e5a02
Serena Sorensen <serena.sorensen7@gmail.com>
2022-04-28 15:09:28 -0600

    set up heroku

--- server/index.js
+++ server/index.js
@@ -1,16 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const {getHouses, deleteHouse, createHouse, updateHouse} = require('./controller.js');
+const {
+  getHouses,
+  deleteHouse,
+  createHouse,
+  updateHouse,
+} = require("./controller.js");
 
-app.get('/api/houses', getHouses);
-app.post('/api/houses', createHouse);
-app.put('/api/houses/:id', updateHouse);
-app.delete('/api/houses/:id', deleteHouse);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
 
-app.listen(4004, () => console.log('listening on 4004'));
+app.get("/api/houses", getHouses);
+app.post("/api/houses", createHouse);
+app.put("/api/houses/:id", updateHouse);
+app.delete("/api/houses/:id", deleteHouse);
+
+const port = process.env.PORT || 4560;
+
+app.listen(port, () => console.log(`listening on ${port}`));

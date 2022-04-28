462e9eded6c3b838412090b1ac2a1d6530031ad6
Serena Sorensen <serena.sorensen7@gmail.com>
2022-04-28 15:55:44 -0600

    fixed baseURL

--- main.js
+++ main.js
@@ -1,41 +1,48 @@
-const housesContainer = document.querySelector('#houses-container')
-const form = document.querySelector('form')
+const housesContainer = document.querySelector("#houses-container");
+const form = document.querySelector("form");
 
-const baseURL = `http://localhost:4004/api/houses`
+const baseURL = `https://ss-traceability-lab-f21.herokuapp.com//api/houses`;
 
-const housesCallback = ({ data: houses }) => displayHouses(houses)
-const errCallback = err => console.log(err)
+const housesCallback = ({ data: houses }) => displayHouses(houses);
+const errCallback = (err) => console.log(err);
 
-const getAllHouses = () => axios.get(baseURL).then(housesCallback).catch(errCallback)
-const createHouse = body => axios.post(baseURL, body).then(housesCallback).catch(errCallback)
-const deleteHouse = id => axios.delete(`${baseURL}/${id}`).then(housesCallback).catch(errCallback)
-const updateHouse = (id, type) => axios.put(`${baseURL}/${id}`, {type}).then(housesCallback).catch(errCallback)
+const getAllHouses = () =>
+  axios.get(baseURL).then(housesCallback).catch(errCallback);
+const createHouse = (body) =>
+  axios.post(baseURL, body).then(housesCallback).catch(errCallback);
+const deleteHouse = (id) =>
+  axios.delete(`${baseURL}/${id}`).then(housesCallback).catch(errCallback);
+const updateHouse = (id, type) =>
+  axios
+    .put(`${baseURL}/${id}`, { type })
+    .then(housesCallback)
+    .catch(errCallback);
 
 function submitHandler(e) {
-    e.preventDefault()
+  e.preventDefault();
 
-    let address = document.querySelector('#address')
-    let price = document.querySelector('#price')
-    let imageURL = document.querySelector('#img')
+  let address = document.querySelector("#address");
+  let price = document.querySelector("#price");
+  let imageURL = document.querySelector("#img");
 
-    let bodyObj = {
-        address: address.value,
-        price: price.value, 
-        imageURL: imageURL.value
-    }
+  let bodyObj = {
+    address: address.value,
+    price: price.value,
+    imageURL: imageURL.value,
+  };
 
-    createHouse(bodyObj)
+  createHouse(bodyObj);
 
-    address.value = ''
-    price.value = ''
-    imageURL.value = ''
+  address.value = "";
+  price.value = "";
+  imageURL.value = "";
 }
 
 function createHouseCard(house) {
-    const houseCard = document.createElement('div')
-    houseCard.classList.add('house-card')
+  const houseCard = document.createElement("div");
+  houseCard.classList.add("house-card");
 
-    houseCard.innerHTML = `<img alt='house cover image' src=${house.imageURL} class="house-cover-image"/>
+  houseCard.innerHTML = `<img alt='house cover image' src=${house.imageURL} class="house-cover-image"/>
     <p class="address">${house.address}</p>
     <div class="btns-container">
         <button onclick="updateHouse(${house.id}, 'minus')">-</button>
@@ -43,19 +50,18 @@ function createHouseCard(house) {
         <button onclick="updateHouse(${house.id}, 'plus')">+</button>
     </div>
     <button onclick="deleteHouse(${house.id})">delete</button>
-    `
+    `;
 
-
-    housesContainer.appendChild(houseCard)
+  housesContainer.appendChild(houseCard);
 }
 
 function displayHouses(arr) {
-    housesContainer.innerHTML = ``
-    for (let i = 0; i < arr.length; i++) {
-        createHouseCard(arr[i])
-    }
+  housesContainer.innerHTML = ``;
+  for (let i = 0; i < arr.length; i++) {
+    createHouseCard(arr[i]);
+  }
 }
 
-form.addEventListener('submit', submitHandler)
+form.addEventListener("submit", submitHandler);
 
-getAllHouses()
+getAllHouses();
